494a9192292f8c8153491c3d534655d31cc78cd2
JoshuaISuper <JoshuaITChao@163.com>
2020-08-05 23:16:19 +0800

    大前端训练营
    1.数据结构学习

--- 大前端训练营/ECMAScript-2015/22-Map数据结构.js
+++ 大前端训练营/ECMAScript-2015/22-Map数据结构.js
@@ -24,3 +24,9 @@ console.log(mp.get(joshua));
 mp.forEach((value, key) => {
     console.log(key, value);
 });
+
+// 弱引用版本 WeakMap
+// 差异就是 Map 中会对所使用到的数据产生引用
+// 即便这个数据在外面被消耗，但是由于 Map 引用了这个数据，所以依然不会回收
+// 而 WeakMap 的特点就是不会产生引用，
+// 一旦数据销毁，就可以被回收，所以不会产生内存泄漏问题。
